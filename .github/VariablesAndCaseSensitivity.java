5896d78092c12135a80ada952073f1030fbb29b4
Erick <johnerickhernandez45@gmail.com>
2025-02-22 20:52:49 +0800

    Submitting OBJPROG Week 02 - Session 01 - Exercise 01

--- .github/VariablesAndCaseSensitivity.java
+++ .github/VariablesAndCaseSensitivity.java
@@ -1,6 +1,5 @@
 public class VariablesAndCaseSensitivity {
     public static void main(String[] args) {
-
 int studentAge = 15;
 double itemPrice = 29.99;
 String studentName = "Alice";
@@ -11,6 +10,5 @@ System.out.println("Item Price: $" + itemPrice);
 System.out.println("Student Name: " + studentName);
 System.out.println("Counter: " + counter);
 System.out.println("Counter (Different Case): " + Counter);
-
     }
 }
